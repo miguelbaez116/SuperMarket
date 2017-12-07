6381cd397c2bc0cb141a9a2a933ed0fc6cac16b0
Mbaez <mbaez@simetricaconsulting.com>
2017-12-07 09:35:56 -0400

    Creando Proyecto

---
+++ MarketOn/MarketOn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarketOn.Startup))]
+namespace MarketOn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
