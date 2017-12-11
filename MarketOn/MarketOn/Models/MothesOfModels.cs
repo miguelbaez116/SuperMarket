5063d72d33b56548f0179bf24fe21b173970e2c9
Miguel Baez lorenzo <miguelbaez116@outlook.es>
2017-12-10 20:55:49 -0400

    Yo nose... taban ahi...

---
+++ MarketOn/MarketOn/Models/MothesOfModels.cs
@@ -0,0 +1,20 @@
+﻿using MarketOn.DataMarketOn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketOn.Models
+{
+    public class MothesOfModels
+    {
+        public Usuario Usuario { get; set; }
+        public Producto Producto { get; set; }
+        public Categoria Categoria { get; set; }
+        public FamiliaCode FamiliaCode { get; set; }
+
+        //Listas
+        public IEnumerable<Producto> ListaProducto { get; set; }
+
+    }
+}
