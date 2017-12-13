38262b0ce23607f54598fb6e21784ede042f1af7
Mbaez <mbaez@simetricaconsulting.com>
2017-12-13 14:08:59 -0400

    Clase anonima y arreglos de inventario ,  Funciones de Eliminar, Modifica, Agregar, Inicio parte de detalle

--- MarketOn/MarketOn/Controllers/HomeController.cs
+++ MarketOn/MarketOn/Controllers/HomeController.cs
@@ -18,14 +18,15 @@ namespace MarketOn.Controllers
             var usuario = HttpContext.GetUsuario();
 
             var db = new DataMarketOn.MarketOnEntities();
+
             var MOM = new MothesOfModels
             {
-                ListaProducto = (from I in db.Inventario
-                                 join U in db.Usuario on I.UserId equals U.UserId
-                                 join F in db.FamiliaCode on I.FamiliaId equals F.FamiliaId
+                ListaProductosInventarios = (from I in db.Inventario
                                  join P in db.Producto on I.ProductoId equals P.ProductoId
-                                 where (U.UserId == usuario.UserId) || (F.FamiliaId == usuario.FamiliaId)
-                                 select P).ToList(),
+                                 where (I.UserId == usuario.UserId) || (I.FamiliaId == usuario.FamiliaId)
+                                 select new ListaProducto() { ProductoNombre = P.ProductoNombre,
+                                                              Cantidad = I.Cantidad,
+                                                              InventarioId = I.InventarioId}).ToList(),
 
                 Usuario = usuario,
                 ListaCategorias = db.Categoria.ToList(),
@@ -34,19 +35,19 @@ namespace MarketOn.Controllers
             };
             return View(MOM);
         }
-
         public ActionResult CrearProducto(string nombreProducto, string categoriaId)
         {
             var db = new DataMarketOn.MarketOnEntities();
-            var idCategoria = Int32.Parse(categoriaId);
             var producto = new Producto();
+
+            var idCategoria = Int32.Parse(categoriaId);
             var validacion = true;
 
             producto.ProductoNombre = nombreProducto;
             producto.CategoriaId = idCategoria;
             producto.Estado = "N/A";
             producto.Precio = 0;
-            producto.Stock = 4;
+            producto.Stock = 0;
 
             db.Producto.Add(producto);
             db.SaveChanges();
@@ -54,32 +55,58 @@ namespace MarketOn.Controllers
             return Json(new { validacion, url = Url.Action("Index", "Home") });
 
         }
-
-        public ActionResult AgregarProducto(string productoId)
+        public ActionResult AgregarProducto(string productoId, string Cantidad)
         {
             var db = new DataMarketOn.MarketOnEntities();
-            var idProducto = Int32.Parse(productoId);
-
             var inventario = new Inventario();
+
+            var idProducto = Int32.Parse(productoId);
+            var cantidad = Decimal.Parse(Cantidad);
             var validacion = true;
             var usuario = HttpContext.GetUsuario();
 
             inventario.ProductoId = idProducto;
             inventario.FamiliaId = usuario.FamiliaId;
             inventario.UserId = usuario.UserId;
-            inventario.Cantidad = 2;
+            inventario.Cantidad = cantidad;
 
             db.Inventario.Add(inventario);
             db.SaveChanges();
 
             return Json(new { validacion, url = Url.Action("Index", "Home") });
+        }
+        public ActionResult EliminarProducto(string inventarioId)
+        {
+            var db = new DataMarketOn.MarketOnEntities();
+            var inventario = new Inventario();
 
+            var idInventario = Int32.Parse(inventarioId);
+            var validacion = true;
+
+            var productoInventario = db.Inventario.Find(idInventario);
+
+            db.Inventario.Remove(productoInventario);
+            db.SaveChanges();
+
+            return Json(new { validacion, url = Url.Action("Index", "Home") });
+        }
+        public ActionResult ModificarProducto(string InventarioId, string Cantidad)
+        {
+            var db = new DataMarketOn.MarketOnEntities();
+            var inventario = new Inventario();
+
+            var idInventario = Int32.Parse(InventarioId);
+            var cantidad = Decimal.Parse(Cantidad);
+            var validacion = true;
+
+            var productoInventario = db.Inventario.Find(idInventario);
+            productoInventario.Cantidad = cantidad;
+
+            db.Entry(productoInventario).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+            return Json(new { validacion, url = Url.Action("Index", "Home") });
         }
 
-        [HttpPost]
-        public ActionResult Index(string nose)
-        {          
-            return View();
-        }
     }
 }
--- MarketOn/MarketOn/Models/MothesOfModels.cs
+++ MarketOn/MarketOn/Models/MothesOfModels.cs
@@ -15,12 +15,20 @@ namespace MarketOn.Models
         public ListaCompra ListaCompra { get; set; }
 
         //Listas
+        public IEnumerable<ListaProducto> ListaProductosInventarios { get; set; }
         public IEnumerable<Producto> ListaProducto { get; set; }
         public IEnumerable<Producto> ListaProductos { get; set; }
         public IEnumerable<ListaCompra> ListaCompraPersonal { get; set; }
         public IEnumerable<ListaCompra> ListaCompraFamiliar { get; set; }
         public IEnumerable<Categoria> ListaCategorias { get; set; }
 
+    }
+
+    public class ListaProducto
+    {
+        public string ProductoNombre { get; set; }
+        public decimal Cantidad { get; set; }
+        public int InventarioId { get; set; }
 
     }
 }
